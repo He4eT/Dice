97aa6c8bb4798f432fcc78e47b392285dc09d1f9
He4eT <He4eT@oddsquat.org>
2025-11-25 13:03:55 +0100

    pkjs: settings: update descriptions

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
     "type": "text",
-    "defaultValue": "Press your watch&rsquo;s right side button to&nbsp;instantly roll a&nbsp;random number from 1&nbsp;to&nbsp;N."
+    "defaultValue": "Press any right-side button on&nbsp;your watch to&nbsp;instantly roll a&nbsp;number from 1&nbsp;to&nbsp;N."
   },
   {
     "type": "section",
     "items": [
       {
         "type": "heading",
-        "defaultValue": "Dice Settings"
+        "defaultValue": "Die Parameters"
       },
       {
         "messageKey": "UPPER_BOUND",
